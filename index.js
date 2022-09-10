725a6636b6c7807efa480f27e01689c942efc380
Dwi Pamungkas <dw.pamungkas@gmail.com>
2022-09-11 05:47:52 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,408 @@
+// const data = [
+//     {
+//       "id": 1,
+//       "name": "Leanne Graham",
+//       "username": "Bret",
+//       "email": "Sincere@april.biz",
+//       "address": {
+//         "street": "Kulas Light",
+//         "suite": "Apt. 556",
+//         "city": "Gwenborough",
+//         "zipcode": "92998-3874",
+//         "geo": {
+//           "lat": "-37.3159",
+//           "lng": "81.1496"
+//         }
+//       },
+//       "phone": "1-770-736-8031 x56442",
+//       "website": "hildegard.org",
+//       "company": {
+//         "name": "Romaguera-Crona",
+//         "catchPhrase": "Multi-layered client-server neural-net",
+//         "bs": "harness real-time e-markets"
+//       }
+//     },
+//     {
+//       "id": 2,
+//       "name": "Ervin Howell",
+//       "username": "Antonette",
+//       "email": "Shanna@melissa.tv",
+//       "address": {
+//         "street": "Victor Plains",
+//         "suite": "Suite 879",
+//         "city": "Wisokyburgh",
+//         "zipcode": "90566-7771",
+//         "geo": {
+//           "lat": "-43.9509",
+//           "lng": "-34.4618"
+//         }
+//       },
+//       "phone": "010-692-6593 x09125",
+//       "website": "anastasia.net",
+//       "company": {
+//         "name": "Deckow-Crist",
+//         "catchPhrase": "Proactive didactic contingency",
+//         "bs": "synergize scalable supply-chains"
+//       }
+//     },
+//     {
+//       "id": 3,
+//       "name": "Clementine Bauch",
+//       "username": "Samantha",
+//       "email": "Nathan@yesenia.net",
+//       "address": {
+//         "street": "Douglas Extension",
+//         "suite": "Suite 847",
+//         "city": "McKenziehaven",
+//         "zipcode": "59590-4157",
+//         "geo": {
+//           "lat": "-68.6102",
+//           "lng": "-47.0653"
+//         }
+//       },
+//       "phone": "1-463-123-4447",
+//       "website": "ramiro.info",
+//       "company": {
+//         "name": "Romaguera-Jacobson",
+//         "catchPhrase": "Face to face bifurcated interface",
+//         "bs": "e-enable strategic applications"
+//       }
+//     },
+//     {
+//       "id": 4,
+//       "name": "Patricia Lebsack",
+//       "username": "Karianne",
+//       "email": "Julianne.OConner@kory.org",
+//       "address": {
+//         "street": "Hoeger Mall",
+//         "suite": "Apt. 692",
+//         "city": "South Elvis",
+//         "zipcode": "53919-4257",
+//         "geo": {
+//           "lat": "29.4572",
+//           "lng": "-164.2990"
+//         }
+//       },
+//       "phone": "493-170-9623 x156",
+//       "website": "kale.biz",
+//       "company": {
+//         "name": "Robel-Corkery",
+//         "catchPhrase": "Multi-tiered zero tolerance productivity",
+//         "bs": "transition cutting-edge web services"
+//       }
+//     },
+//     {
+//       "id": 5,
+//       "name": "Chelsey Dietrich",
+//       "username": "Kamren",
+//       "email": "Lucio_Hettinger@annie.ca",
+//       "address": {
+//         "street": "Skiles Walks",
+//         "suite": "Suite 351",
+//         "city": "Roscoeview",
+//         "zipcode": "33263",
+//         "geo": {
+//           "lat": "-31.8129",
+//           "lng": "62.5342"
+//         }
+//       },
+//       "phone": "(254)954-1289",
+//       "website": "demarco.info",
+//       "company": {
+//         "name": "Keebler LLC",
+//         "catchPhrase": "User-centric fault-tolerant solution",
+//         "bs": "revolutionize end-to-end systems"
+//       }
+//     },
+//     {
+//       "id": 6,
+//       "name": "Mrs. Dennis Schulist",
+//       "username": "Leopoldo_Corkery",
+//       "email": "Karley_Dach@jasper.info",
+//       "address": {
+//         "street": "Norberto Crossing",
+//         "suite": "Apt. 950",
+//         "city": "South Christy",
+//         "zipcode": "23505-1337",
+//         "geo": {
+//           "lat": "-71.4197",
+//           "lng": "71.7478"
+//         }
+//       },
+//       "phone": "1-477-935-8478 x6430",
+//       "website": "ola.org",
+//       "company": {
+//         "name": "Considine-Lockman",
+//         "catchPhrase": "Synchronised bottom-line interface",
+//         "bs": "e-enable innovative applications"
+//       }
+//     },
+//     {
+//       "id": 7,
+//       "name": "Kurtis Weissnat",
+//       "username": "Elwyn.Skiles",
+//       "email": "Telly.Hoeger@billy.biz",
+//       "address": {
+//         "street": "Rex Trail",
+//         "suite": "Suite 280",
+//         "city": "Howemouth",
+//         "zipcode": "58804-1099",
+//         "geo": {
+//           "lat": "24.8918",
+//           "lng": "21.8984"
+//         }
+//       },
+//       "phone": "210.067.6132",
+//       "website": "elvis.io",
+//       "company": {
+//         "name": "Johns Group",
+//         "catchPhrase": "Configurable multimedia task-force",
+//         "bs": "generate enterprise e-tailers"
+//       }
+//     },
+//     {
+//       "id": 8,
+//       "name": "Nicholas Runolfsdottir V",
+//       "username": "Maxime_Nienow",
+//       "email": "Sherwood@rosamond.me",
+//       "address": {
+//         "street": "Ellsworth Summit",
+//         "suite": "Suite 729",
+//         "city": "Aliyaview",
+//         "zipcode": "45169",
+//         "geo": {
+//           "lat": "-14.3990",
+//           "lng": "-120.7677"
+//         }
+//       },
+//       "phone": "586.493.6943 x140",
+//       "website": "jacynthe.com",
+//       "company": {
+//         "name": "Abernathy Group",
+//         "catchPhrase": "Implemented secondary concept",
+//         "bs": "e-enable extensible e-tailers"
+//       }
+//     },
+//     {
+//       "id": 9,
+//       "name": "Glenna Reichert",
+//       "username": "Delphine",
+//       "email": "Chaim_McDermott@dana.io",
+//       "address": {
+//         "street": "Dayna Park",
+//         "suite": "Suite 449",
+//         "city": "Bartholomebury",
+//         "zipcode": "76495-3109",
+//         "geo": {
+//           "lat": "24.6463",
+//           "lng": "-168.8889"
+//         }
+//       },
+//       "phone": "(775)976-6794 x41206",
+//       "website": "conrad.com",
+//       "company": {
+//         "name": "Yost and Sons",
+//         "catchPhrase": "Switchable contextually-based project",
+//         "bs": "aggregate real-time technologies"
+//       }
+//     },
+//     {
+//       "id": 10,
+//       "name": "Clementina DuBuque",
+//       "username": "Moriah.Stanton",
+//       "email": "Rey.Padberg@karina.biz",
+//       "address": {
+//         "street": "Kattie Turnpike",
+//         "suite": "Suite 198",
+//         "city": "Lebsackbury",
+//         "zipcode": "31428-2261",
+//         "geo": {
+//           "lat": "-38.2386",
+//           "lng": "57.2232"
+//         }
+//       },
+//       "phone": "024-648-3804",
+//       "website": "ambrose.net",
+//       "company": {
+//         "name": "Hoeger LLC",
+//         "catchPhrase": "Centralized empowering task-force",
+//         "bs": "target end-to-end models"
+//       }
+//     }
+// ]
+
+//   for(i = 0; i < data.length; i++){
+//     console.log(data[i].name)
+//   }
+
+//   for(i = 0; i < data.length; i++){
+//     if(data[i].id % 2 === 0){
+//         console.log(data[i].name)
+//     }else{
+//         'bukan genap'
+//     }
+//   }
+
+
+//   const newData = []
+//   for(i = 0; i < data.length; i++){
+//     let object ={
+//         name: data[i].name,
+//         email: data[i].email
+//     }
+//     newData.push(object)
+//   }
+//   console.log(newData)
+
+
+// const cars2 = [
+// ['agya', 'ayla'],
+// ['sigra', 'cayla'],
+// ['avanza', 'veloz']
+// ]
+
+// const dataBaru = cars2.map((item) => {
+// return item[1]
+// })
+// console.log(dataBaru)
+
+
+
+//DECLARATION FUNCTION
+// function kuadrat(angka1, angka2){
+//     let result = Math.pow(angka1, angka2)
+//     return result
+// }
+// console.log(kuadrat(4,5))
+
+
+
+//EXPRESSION FUNCTION
+// const kuadrat2 = (angka1,angka2) => {
+//     let result = Math.pow(angka1, angka2)
+//     return `Hasil dari ${angka1} kuadrat ${angka2} adalah ${result}`
+// }
+// console.log(kuadrat2(4,5))
+
+
+
+//LOOP NAMA DAN USIA
+// const data = [
+//     {
+//         id: 1,
+//         name: "cerulean",
+//         year: 2000,
+//         color: "#98B2D1",
+//         pantone_value: "15-4020",
+//       },
+//       {
+//         id: 2,
+//         name: "fuchsia rose",
+//         year: 2001,
+//         color: "#C74375",
+//         pantone_value: "17-2031",
+//       },
+//       {
+//         id: 3,
+//         name: "true red",
+//         year: 2002,
+//         color: "#BF1932",
+//         pantone_value: "19-1664",
+//       },
+//       {
+//         id: 4,
+//         name: "aqua sky",
+//         year: 2003,
+//         color: "#7BC4C4",
+//         pantone_value: "14-4811",
+//       },
+//       {
+//         id: 5,
+//         name: "tigerlily",
+//         year: 2004,
+//         color: "#E2583E",
+//         pantone_value: "17-1456",
+//       },
+//       {
+//         id: 6,
+//         name: "blue turquoise",
+//         year: 2005,
+//         color: "#53B0AE",
+//         pantone_value: "15-5217",
+//       },
+// ]
+
+// const dataBaru = data.map((item) => {
+//     let object = {
+//         name: item.name,
+//         age: 2022 - item.year
+//     }
+//     data.push(object)
+//     return data
+// })
+// console.log(dataBaru)
+
+
+// const dataBaru = (arr) =>{
+//     const newData = arr.map((item) => {
+//         let object = {
+//             name: item.name,
+//             age: 2022 - item.year
+//         }
+//         return object
+//     })
+//     return newData 
+// }
+// console.log(dataBaru(data))
+
+
+// function findLongestWord(str) {
+//   var strSplit = str.split(' ');
+//   var longestWord = strSplit.sort(function(a,b){
+//     return b.length - a.length
+//   });
+//   return longestWord[0]
+// }
+// //findLongestWord("The quick brown fox jumped over the lazy dog")
+// console.log(findLongestWord("The quick brown fox jumped over the lazy dog"))
+
+
+
+// function StringChallenge(str) {
+//   const UPPER = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+//   const LOWER = 'abcdefghijklmnopqrstuvwxyz'
+//   const result = []
+
+//   for(let i = 0; i < str.length; i++)
+//   {
+//     if(UPPER.indexOf(str[i]) !== -1)
+//     {
+//       result.push(str[i].toLowerCase())
+//     }
+//     else if(LOWER.indexOf(str[i]) !== -1)
+//     {
+//       result.push(str[i].toUpperCase())
+//     }
+//     else 
+//     {
+//       result.push(str[i])
+//     }
+//   }
+//   // code goes here  
+//   return result.join(''); 
+
+// }
+
+// console.log(StringChallenge('Jump'))
+   
+// keep this function call here 
+//console.log(StringChallenge(readline()));
+
+const guess = 4
+const sum = [];
+for (let i = 1; i <= guess; i++) {
+    sum += i;
+}
+console.log(sum)
+
+
